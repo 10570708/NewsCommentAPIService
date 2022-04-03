6037e1e35d526e5eea4274b783d7bde68cabb069
10570708 <83276752+10570708@users.noreply.github.com>
2022-04-03 20:54:33 +0100

    initial commit

---
+++ Data/NewsCommentAPIServiceContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NewsCommentAPIService.Models;
+
+namespace NewsCommentAPIService.Data
+{
+    public class NewsCommentAPIServiceContext : DbContext
+    {
+        public NewsCommentAPIServiceContext (DbContextOptions<NewsCommentAPIServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NewsCommentAPIService.Models.Comment> Comment { get; set; }
+    }
+}
---
+++ Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsCommentAPIService.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int ReportId { get; set; }
+        public Guid CreatedBy { get; set; }
+        public string CommentText { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedDate { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime UpdatedDate { get; set; }
+
+    }
+}
